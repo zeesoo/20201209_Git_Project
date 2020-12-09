f293baccaa91e2ef8039dfaa530dbd0cde7ee7cc
dudtj <dudtj@DESKTOP-CFHQQL3.skbroadband>
2020-12-09 14:01:50 +0900

    main수정_영서

--- 20201209_together/src/com/min/edu/GitMain.java
+++ 20201209_together/src/com/min/edu/GitMain.java
@@ -4,7 +4,7 @@ public class GitMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		System.out.println("안녕하세요 이제 이곳만 보시면 됩니다. 이제부터 main에서만 작업을 수행하겟습니다. 자유롭게 치셔도 됩니다.");
 	}
 
 }
